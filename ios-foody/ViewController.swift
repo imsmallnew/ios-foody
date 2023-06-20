39402777327bdf13345601b015cf03350b0c7df2
imsmallnew <imsmallnew@gmail.com>
2023-06-20 21:13:41 +0800

    Initial Commit

---
+++ ios-foody/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-foody
+//
+//  Created by Daniel-PC on 2023/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
